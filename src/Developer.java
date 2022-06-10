32752d19992d5e5f1a70d42113abfde903c6ff21
karthi <karthi@karthi-Lenovo-E41-25>
2022-06-10 11:22:50 +0530

    Developer commit

---
+++ src/Developer.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Developer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Name Here ");
+		String name = sc.next();
+		System.out.println("Java Developer Name is "+name);
+
+	}
+
+}
